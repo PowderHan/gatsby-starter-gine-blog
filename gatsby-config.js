2f378f1020eacaa5d614d86f615fa787a9e0a85f
Sylvan Ricardo <46041980+PowderHan@users.noreply.github.com>
2021-01-26 20:48:46 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,14 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/sylvan18/3590cf9521394c3982128e85d49f6c4e?v=22072bd1113845f0b1355b19bd2124fd"
+               
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/sylvan18/5774def63dba4af087b726d7126fc4fa?v=5eba4c35d69c43d49c81758d69ec9d61"
             }
         }
     ],
